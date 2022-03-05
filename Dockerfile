7e9d3a2360414d6bc8d9d8b40336713b1ffa9e55
Shubham Pandey <shubham9411@gmail.com>
2022-03-05 22:13:35 +0530

    Docker for prod

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM golang:1.17.6 as builder
+# Define build env
+ENV GOOS linux
+ENV CGO_ENABLED 0
+# Add a work directory
+WORKDIR /app
+# Cache and install dependencies
+COPY go.mod go.sum ./
+RUN go mod download
+# Copy app files
+COPY . .
+# Build app
+RUN go build -o app
+
+FROM alpine:3.14 as production
+# Add certificates
+RUN apk add --no-cache ca-certificates
+# Copy built binary from builder
+COPY --from=builder app .
+# Expose port
+EXPOSE 3000
+# Exec built binary
+CMD ./app
--- config/connection.go
+++ config/connection.go
@@ -14,20 +14,21 @@ func Connect() *gorm.DB {
 	user := make(chan string, 1)
 	pass := make(chan string, 1)
 	dbname := make(chan string, 1)
+	host := make(chan string, 1)
 
 	if os.Getenv("GO_ENV") != "production" {
 		user <- utils.GodotEnv("DB_USER")
 		pass <- utils.GodotEnv("DB_PASSWORD")
 		dbname <- utils.GodotEnv("DB_NAME")
+		host <- utils.GodotEnv("DB_HOST")
 	} else {
 		user <- os.Getenv("DB_USER")
 		pass <- os.Getenv("DB_PASSWORD")
 		dbname <- os.Getenv("DB_NAME")
+		host <- os.Getenv("DB_HOST")
 	}
 
-	// db, err := gorm.Open(postgres.Open(<-databaseURI), &gorm.Config{})
-	db, err := gorm.Open("mysql", fmt.Sprintf("%s:%s@/%s?charset=utf8mb4&parseTime=True&loc=Local", <-user, <-pass, <-dbname))
-
+	db, err := gorm.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", <-user, <-pass, <-host, <-dbname))
 	if err != nil {
 		log.Fatal("Error in connecting db :::", err)
 	}
